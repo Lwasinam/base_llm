3594f158b995d1ef778976e3211d0969ef0752c8
Lwasinam <lwasinamdilli@gmail.com>
2024-01-10 13:12:23 +0100

    data

--- train.py
+++ train.py
@@ -150,7 +150,7 @@ def get_or_build_tokenizer(config, ds, lang):
 
 def get_ds(config):
     # It only has the train split, so we divide it overselves
-    ds_raw = load_dataset('Lwasinam/en-ha', 
+    ds_raw = load_dataset("castorini/wura", 
     # f"{config['lang_src']}-{config['lang_tgt']}",
      split='train')
     print(ds_raw[0])
